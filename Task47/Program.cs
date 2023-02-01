313d3c663f1d8e86e1bd98fdcb95800f6364fd79
Tatiana Denisova <2746619@gmail.com>
2023-02-02 00:20:09 +0300

    Add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/ 
+
+int getDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+double[,] get2DDoubleArray(int rowLength, int colLength, int start, int end)
+{
+    double[,] array = new double[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, end + 1) + Math.Round(new Random().NextDouble(), 1);
+        }
+    }
+    return array;
+}
+void print2DArray(double[,] array)
+{
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int n = getDataFromUser("Введите количество строк");
+int m = getDataFromUser("Введите количество столбцов");
+double[,] array = get2DDoubleArray(n, m, 0, 10);
+print2DArray(array);
